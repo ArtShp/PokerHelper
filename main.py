62a7380bc61d9aee76685c0ae700bc2ef45106a5
Артём <56469391+ArtShp@users.noreply.github.com>
2020-10-06 16:09:02 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from poker_hand import PokerHand
+
+instance = PokerHand('3H 7D 2S AS AD')
+instance.show_hand()
---
+++ poker_hand.py
@@ -0,0 +1,42 @@
+from copy import deepcopy
+
+class PokerHand(object):
+    RESULT = ["Loss", "Tie", "Win"]
+    values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+    suits = ['S', 'H', 'D', 'C']
+
+    def __init__(self, hand):
+        self.hand = hand.split()
+        self._sort_hand()
+
+    def show_hand(self):
+        print(self.hand)
+
+    def _sort_hand(self):
+        hand = deepcopy(self.hand)
+        arr_i = [hand[0][0], hand[1][0], hand[2][0], hand[3][0], hand[4][0]]
+        arr = []
+        for i in range(5):
+            arr.append(self.values.index(arr_i[i]))
+        arr.sort()
+        arr = [self.values[arr[0]], self.values[arr[1]], self.values[arr[2]], self.values[arr[3]], self.values[arr[4]]]
+        fin_arr = []
+        for i in range(5):
+            fin_arr.append(hand[int(arr_i.index(arr[i]))])
+            hand.remove(hand[int(arr_i.index(arr[i]))])
+            arr_i.remove(arr[i])
+        self.hand = fin_arr
+
+    def _count_value(self):
+        self.points = 0
+
+        if (('T' in [self.hand[0][0], self.hand[1][0], self.hand[2][0], self.hand[3][0], self.hand[4][0]]) and (
+                'J' in [self.hand[0][0], self.hand[1][0], self.hand[2][0], self.hand[3][0], self.hand[4][0]]) and (
+                    'Q' in [self.hand[0][0], self.hand[1][0], self.hand[2][0], self.hand[3][0], self.hand[4][0]]) and (
+                    'K' in [self.hand[0][0], self.hand[1][0], self.hand[2][0], self.hand[3][0], self.hand[4][0]]) and (
+                    'A' in [self.hand[0][0], self.hand[1][0], self.hand[2][0], self.hand[3][0], self.hand[4][0]])) and (
+                self.hand[0][1] == self.hand[1][1] == self.hand[2][1] == self.hand[3][1] == self.hand[4][1]):
+            self.points = 10
+
+    def compare_with(self, other):
+        pass
