53cefbb36a2e2a48c46f46d9c4c2cf37d7ca663f
Артём <56469391+ArtShp@users.noreply.github.com>
2020-10-16 17:16:45 +0300

    Some cosmetic changes

--- poker_hand.py
+++ poker_hand.py
@@ -1,10 +1,10 @@
 class PokerHand(object):
     RESULT = ["Win", "Tie", "Loss"]
-    values = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
+    VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A']
 
     def __init__(self, hand):
         self.hand = hand.split()
-        self.highcard_exception = ''
+        self.hce1 = ''
         self._sort_hand()
         self._count_value()
 
@@ -15,9 +15,9 @@ class PokerHand(object):
         arr_i = [self.hand[0][0], self.hand[1][0], self.hand[2][0], self.hand[3][0], self.hand[4][0]]
         arr = []
         for i in range(5):
-            arr.append(self.values.index(arr_i[i]))
+            arr.append(self.VALUES.index(arr_i[i]))
         arr.sort()
-        arr = [self.values[arr[0]], self.values[arr[1]], self.values[arr[2]], self.values[arr[3]], self.values[arr[4]]]
+        arr = [self.VALUES[arr[0]], self.VALUES[arr[1]], self.VALUES[arr[2]], self.VALUES[arr[3]], self.VALUES[arr[4]]]
         fin_arr = []
         for i in range(5):
             fin_arr.append(self.hand[int(arr_i.index(arr[i]))])
@@ -27,10 +27,7 @@ class PokerHand(object):
 
     def _count_value(self):
         self.points = 0
-        self.highcard_exception2 = ''
-        self.highcard_exception3 = ''
-        self.highcard_exception4 = ''
-        self.highcard_exception5 = ''
+        self.hce1, self.hce2, self.hce3, self.hce4, self.hce5 = ('' for i in range(5))
         if 'T' == self.hand[0][0] and \
                 'J' == self.hand[1][0] and \
                 'Q' == self.hand[2][0] and \
@@ -38,126 +35,126 @@ class PokerHand(object):
                 'A' == self.hand[4][0] and \
                 self.hand[0][1] == self.hand[1][1] == self.hand[2][1] == self.hand[3][1] == self.hand[4][1]:
             self.points = 10
-            self.highcard_exception = self.values.index(self.hand[4][0])
+            self.hce1 = self.VALUES.index(self.hand[4][0])
         else:
             for i in range(9):
-                if self.values[i] == self.hand[0][0] and \
-                        self.values[i+1] == self.hand[1][0] and \
-                        self.values[i+2] == self.hand[2][0] and \
-                        self.values[i+3] == self.hand[3][0] and \
-                        self.values[i+4] == self.hand[4][0] and \
+                if self.VALUES[i] == self.hand[0][0] and \
+                        self.VALUES[i+1] == self.hand[1][0] and \
+                        self.VALUES[i+2] == self.hand[2][0] and \
+                        self.VALUES[i+3] == self.hand[3][0] and \
+                        self.VALUES[i+4] == self.hand[4][0] and \
                         self.hand[0][1] == self.hand[1][1] == self.hand[2][1] == self.hand[3][1] == self.hand[4][1]:
                     self.points = 9
-                    self.highcard_exception = self.values.index(self.hand[4][0])
+                    self.hce1 = self.VALUES.index(self.hand[4][0])
                     break
             if self.points == 0:
                 if self.hand[0][0] == self.hand[1][0] == self.hand[2][0] == self.hand[3][0]:
                     self.points = 8
-                    self.highcard_exception = self.values.index(self.hand[0][0])
-                    self.highcard_exception2 = self.values.index(self.hand[4][0])
+                    self.hce1 = self.VALUES.index(self.hand[0][0])
+                    self.hce2 = self.VALUES.index(self.hand[4][0])
                 elif self.hand[1][0] == self.hand[2][0] == self.hand[3][0] == self.hand[4][0]:
                     self.points = 8
-                    self.highcard_exception = self.values.index(self.hand[1][0])
-                    self.highcard_exception2 = self.values.index(self.hand[0][0])
+                    self.hce1 = self.VALUES.index(self.hand[1][0])
+                    self.hce2 = self.VALUES.index(self.hand[0][0])
                 elif self.hand[0][0] == self.hand[1][0] == self.hand[2][0] and self.hand[3][0] == self.hand[4][0]:
                     self.points = 7
-                    self.highcard_exception = self.values.index(self.hand[0][0])
-                    self.highcard_exception2 = self.values.index(self.hand[3][0])
+                    self.hce1 = self.VALUES.index(self.hand[0][0])
+                    self.hce2 = self.VALUES.index(self.hand[3][0])
                 elif self.hand[0][0] == self.hand[1][0] and self.hand[2][0] == self.hand[3][0] == self.hand[4][0]:
                     self.points = 7
-                    self.highcard_exception = self.values.index(self.hand[2][0])
-                    self.highcard_exception2 = self.values.index(self.hand[0][0])
+                    self.hce1 = self.VALUES.index(self.hand[2][0])
+                    self.hce2 = self.VALUES.index(self.hand[0][0])
                 elif self.hand[0][1] == self.hand[1][1] == self.hand[2][1] == self.hand[3][1] == self.hand[4][1]:
                     self.points = 6
-                    self.highcard_exception = self.values.index(self.hand[4][0])
-                    self.highcard_exception2 = self.values.index(self.hand[3][0])
-                    self.highcard_exception3 = self.values.index(self.hand[2][0])
-                    self.highcard_exception4 = self.values.index(self.hand[1][0])
-                    self.highcard_exception5 = self.values.index(self.hand[0][0])
+                    self.hce1 = self.VALUES.index(self.hand[4][0])
+                    self.hce2 = self.VALUES.index(self.hand[3][0])
+                    self.hce3 = self.VALUES.index(self.hand[2][0])
+                    self.hce4 = self.VALUES.index(self.hand[1][0])
+                    self.hce5 = self.VALUES.index(self.hand[0][0])
                 else:
                     for i in range(9):
-                        if self.values[i] == self.hand[0][0] and \
-                                self.values[i+1] == self.hand[1][0] and \
-                                self.values[i+2] == self.hand[2][0] and \
-                                self.values[i+3] == self.hand[3][0] and \
-                                self.values[i+4] == self.hand[4][0]:
+                        if self.VALUES[i] == self.hand[0][0] and \
+                                self.VALUES[i+1] == self.hand[1][0] and \
+                                self.VALUES[i+2] == self.hand[2][0] and \
+                                self.VALUES[i+3] == self.hand[3][0] and \
+                                self.VALUES[i+4] == self.hand[4][0]:
                             self.points = 5
-                            self.highcard_exception = self.values.index(self.hand[4][0])
+                            self.hce1 = self.VALUES.index(self.hand[4][0])
                             break
                     if self.points == 0:
                         if self.hand[0][0] == self.hand[1][0] == self.hand[2][0]:
                             self.points = 4
-                            self.highcard_exception = self.values.index(self.hand[0][0])
-                            self.highcard_exception2 = self.values.index(self.hand[4][0])
-                            self.highcard_exception3 = self.values.index(self.hand[3][0])
+                            self.hce1 = self.VALUES.index(self.hand[0][0])
+                            self.hce2 = self.VALUES.index(self.hand[4][0])
+                            self.hce3 = self.VALUES.index(self.hand[3][0])
                         elif self.hand[1][0] == self.hand[2][0] == self.hand[3][0]:
                             self.points = 4
-                            self.highcard_exception = self.values.index(self.hand[1][0])
-                            self.highcard_exception2 = self.values.index(self.hand[4][0])
-                            self.highcard_exception3 = self.values.index(self.hand[0][0])
+                            self.hce1 = self.VALUES.index(self.hand[1][0])
+                            self.hce2 = self.VALUES.index(self.hand[4][0])
+                            self.hce3 = self.VALUES.index(self.hand[0][0])
                         elif self.hand[2][0] == self.hand[3][0] == self.hand[4][0]:
                             self.points = 4
-                            self.highcard_exception = self.values.index(self.hand[2][0])
-                            self.highcard_exception2 = self.values.index(self.hand[1][0])
-                            self.highcard_exception3 = self.values.index(self.hand[0][0])
+                            self.hce1 = self.VALUES.index(self.hand[2][0])
+                            self.hce2 = self.VALUES.index(self.hand[1][0])
+                            self.hce3 = self.VALUES.index(self.hand[0][0])
                         elif self.hand[0][0] == self.hand[1][0] and self.hand[2][0] == self.hand[3][0]:
                             self.points = 3
-                            self.highcard_exception = self.values.index(self.hand[2][0])
-                            self.highcard_exception2 = self.values.index(self.hand[0][0])
-                            self.highcard_exception3 = self.values.index(self.hand[4][0])
+                            self.hce1 = self.VALUES.index(self.hand[2][0])
+                            self.hce2 = self.VALUES.index(self.hand[0][0])
+                            self.hce3 = self.VALUES.index(self.hand[4][0])
                         elif self.hand[0][0] == self.hand[1][0] and self.hand[3][0] == self.hand[4][0]:
                             self.points = 3
-                            self.highcard_exception = self.values.index(self.hand[3][0])
-                            self.highcard_exception2 = self.values.index(self.hand[0][0])
-                            self.highcard_exception3 = self.values.index(self.hand[2][0])
+                            self.hce1 = self.VALUES.index(self.hand[3][0])
+                            self.hce2 = self.VALUES.index(self.hand[0][0])
+                            self.hce3 = self.VALUES.index(self.hand[2][0])
                         elif self.hand[1][0] == self.hand[2][0] and self.hand[3][0] == self.hand[4][0]:
                             self.points = 3
-                            self.highcard_exception = self.values.index(self.hand[3][0])
-                            self.highcard_exception2 = self.values.index(self.hand[1][0])
-                            self.highcard_exception3 = self.values.index(self.hand[0][0])
+                            self.hce1 = self.VALUES.index(self.hand[3][0])
+                            self.hce2 = self.VALUES.index(self.hand[1][0])
+                            self.hce3 = self.VALUES.index(self.hand[0][0])
                         elif self.hand[0][0] == self.hand[1][0]:
                             self.points = 2
-                            self.highcard_exception = self.values.index(self.hand[0][0])
-                            self.highcard_exception2 = self.values.index(self.hand[4][0])
-                            self.highcard_exception3 = self.values.index(self.hand[3][0])
-                            self.highcard_exception4 = self.values.index(self.hand[2][0])
+                            self.hce1 = self.VALUES.index(self.hand[0][0])
+                            self.hce2 = self.VALUES.index(self.hand[4][0])
+                            self.hce3 = self.VALUES.index(self.hand[3][0])
+                            self.hce4 = self.VALUES.index(self.hand[2][0])
                         elif self.hand[1][0] == self.hand[2][0]:
                             self.points = 2
-                            self.highcard_exception = self.values.index(self.hand[1][0])
-                            self.highcard_exception2 = self.values.index(self.hand[4][0])
-                            self.highcard_exception3 = self.values.index(self.hand[3][0])
-                            self.highcard_exception4 = self.values.index(self.hand[0][0])
+                            self.hce1 = self.VALUES.index(self.hand[1][0])
+                            self.hce2 = self.VALUES.index(self.hand[4][0])
+                            self.hce3 = self.VALUES.index(self.hand[3][0])
+                            self.hce4 = self.VALUES.index(self.hand[0][0])
                         elif self.hand[2][0] == self.hand[3][0]:
                             self.points = 2
-                            self.highcard_exception = self.values.index(self.hand[2][0])
-                            self.highcard_exception2 = self.values.index(self.hand[4][0])
-                            self.highcard_exception3 = self.values.index(self.hand[1][0])
-                            self.highcard_exception4 = self.values.index(self.hand[0][0])
+                            self.hce1 = self.VALUES.index(self.hand[2][0])
+                            self.hce2 = self.VALUES.index(self.hand[4][0])
+                            self.hce3 = self.VALUES.index(self.hand[1][0])
+                            self.hce4 = self.VALUES.index(self.hand[0][0])
                         elif self.hand[3][0] == self.hand[4][0]:
                             self.points = 2
-                            self.highcard_exception = self.values.index(self.hand[3][0])
-                            self.highcard_exception2 = self.values.index(self.hand[2][0])
-                            self.highcard_exception3 = self.values.index(self.hand[1][0])
-                            self.highcard_exception4 = self.values.index(self.hand[0][0])
+                            self.hce1 = self.VALUES.index(self.hand[3][0])
+                            self.hce2 = self.VALUES.index(self.hand[2][0])
+                            self.hce3 = self.VALUES.index(self.hand[1][0])
+                            self.hce4 = self.VALUES.index(self.hand[0][0])
                         else:
                             self.points = 1
-                            self.highcard_exception = self.values.index(self.hand[4][0])
-                            self.highcard_exception2 = self.values.index(self.hand[3][0])
-                            self.highcard_exception3 = self.values.index(self.hand[2][0])
-                            self.highcard_exception4 = self.values.index(self.hand[1][0])
-                            self.highcard_exception5 = self.values.index(self.hand[0][0])
+                            self.hce1 = self.VALUES.index(self.hand[4][0])
+                            self.hce2 = self.VALUES.index(self.hand[3][0])
+                            self.hce3 = self.VALUES.index(self.hand[2][0])
+                            self.hce4 = self.VALUES.index(self.hand[1][0])
+                            self.hce5 = self.VALUES.index(self.hand[0][0])
 
     def compare_with(self, other):
         pts1, pts2 = self.points, other._get_points()
-        hce1, hce2 = self.highcard_exception, other.highcard_exception
-        hce1_2, hce2_2 = self.highcard_exception2, other.highcard_exception2
-        hce1_3, hce2_3 = self.highcard_exception3, other.highcard_exception3
-        hce1_4, hce2_4 = self.highcard_exception4, other.highcard_exception4
-        hce1_5, hce2_5 = self.highcard_exception5, other.highcard_exception5
+        hce1_1, hce2_1 = self.hce1, other.hce1
+        hce1_2, hce2_2 = self.hce2, other.hce2
+        hce1_3, hce2_3 = self.hce3, other.hce3
+        hce1_4, hce2_4 = self.hce4, other.hce4
+        hce1_5, hce2_5 = self.hce5, other.hce5
         if pts1 == pts2:
-            if hce1 > hce2:
+            if hce1_1 > hce2_2:
                 return self.RESULT[0]
-            elif hce1 == hce2:
+            elif hce1_1 == hce2_1:
                 if hce1_2 > hce2_2:
                     return self.RESULT[0]
                 elif hce1_2 == hce2_2:
